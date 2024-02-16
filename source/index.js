d9504abdb7a0d6a93d047c80a29d954f96562bfb
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-02-16 16:06:28 +0530

    able to generate shortURL and redirect to original when accessed

--- source/index.js
+++ source/index.js
@@ -3,6 +3,13 @@ const cors = require("cors")
 const PORT = process.env.PORT || 8080
 const path = require("path");
 const { connection } = require("./db");
+const { URLModel } = require("./models/url.model");
+const base62 = require('base62');
+
+
+
+let count = 10000000000;
+console.log(base62.encode(count));
 const app = express();
 
 app.use(cors());
@@ -12,6 +19,42 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/index.html"))
 })
 
+app.post("/original",async(req,res)=>{
+    try {
+        let {originalURL} = req.body
+        let shortURL = base62.encode(count);
+        count++;
+        console.log(count)
+        let content = {
+            originalURL:originalURL,
+            shortURL:shortURL
+        }
+        let newURL = new URLModel(content)
+        await newURL.save()
+        res.status(200).json({"new URL":`http://localhost:8080/${shortURL}`})
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({Error:"Error when shortening URL"})
+    }
+})
+
+app.get("/:shortURL",async(req,res)=>{
+    try {
+        let shortURL = req.params.shortURL
+        let entry = await URLModel.findOne({shortURL:shortURL})
+        
+        if(entry){
+            let originalURL = entry.originalURL
+        res.redirect(originalURL)
+        }
+        else{
+            res.send("URL not found")
+        }
+        
+    } catch (error) {
+        res.status(400).send("error when fetching original URL")
+    }
+})
 
 
 
